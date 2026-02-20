b7ac7e35ed855441e27f22230c401781506ff36f
pokemans123 <pranavarunkumar2010@gmail.com>
2026-02-19 20:55:38 -0500

    further improved nvim IDE and updated file manager

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
 	  "williamboman/mason-lspconfig.nvim",
 	  config = function()
 	       require("mason-lspconfig").setup({
-		    ensure_installed = {"lua_ls","pyright","codebook"}
+		    ensure_installed = {"lua_ls","clangd","pyright","codebook","stylua","ruff","ast_grep"}
 	       })
 	  end
      },
@@ -19,6 +19,9 @@ return {
 	       vim.lsp.enable("lua_ls")
 	       vim.lsp.enable("pyright")
 	       vim.lsp.enable("codebook")
+	       vim.lsp.enable("stylua")
+	       vim.lsp.enable("ruff")
+	       vim.lsp.enable("clangd")
 	       vim.keymap.set('n','K',vim.lsp.buf.hover, {})
 	       vim.keymap.set('n','gD',vim.lsp.buf.declaration, {})
 	       vim.keymap.set({ 'n','v' }, '<leader>ca', vim.lsp.buf.code_action, {})
